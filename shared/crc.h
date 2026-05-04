6c29f7eea3e9d27c24e81921a8c808f7aeb049bc
SoucaThomas <thomassouca@gmail.com>
2026-05-04 16:42:55 +0300

    feat(crc16): implemented CRC-16-CCITT

---
+++ shared/crc.h
@@ -0,0 +1,25 @@
+#ifndef CRC_H
+#define CRC_H
+
+#include <stdint.h>
+
+inline uint16_t crc16(const uint8_t* data, uint16_t length) {
+  uint16_t crc = 0xFFFF;
+  uint8_t i;
+
+  while (length--) {
+    crc = crc ^ ((uint16_t)*data++ << 8);
+    i = 8;
+    do {
+      if (crc & 0x8000) {
+        crc = crc << 1 ^ 0x1021;
+      } else {
+        crc = crc << 1;
+      }
+    } while (--i);
+  }
+
+  return crc;
+}
+
+#endif
